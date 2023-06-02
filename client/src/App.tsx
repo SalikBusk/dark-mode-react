43ecc8b3fa3c9034c8183697cf5f6ae23bc79b0c
salikbubu <salikbula@gmail.com>
2023-06-02 12:24:51 +0200

    slut

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [theme, setTheme] = useState('light')
+
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light')
+    }
+  };
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   return (
-    <div className="App">
-      
+    <div className={`${theme}`}>
+      <section className='w-full h-screen flex flex-col items-center justify-center'>
+        <button onClick={toggleTheme} className="bg-rose-500 py-[10px] px-[20px] rounded-[10px]">Toggle Theme</button>
+        <h1>Hello world</h1>
+      </section>
     </div>
   );
 }
